bc0f64bca51edfa604a553ad5244e18b8d915dc3
Makara Aerospace & Avionics Team TRUI <82041492+MaatTrui@users.noreply.github.com>
2021-07-16 15:23:09 +0800

    Add files via upload

--- Kodingan roket/roket.py
+++ Kodingan roket/roket.py
@@ -1,34 +1,28 @@
-import sys
 from i2c_adxl345 import *
 from i2c_itg3205 import *
 import multiprocessing
-from time import sleep, time
-
+from time import sleep, gmtime, strftime
 import serial 
-from micropyGPS import MicropyGPS
 from digi.xbee.devices import XBeeDevice
 from math import atan, sqrt
-from bitarray import bitarray
 from py_qmc5883l import *
 from picamera import PiCamera
-import numpy as np
+from numpy import empty, uint8
 import simplejpeg
+import re
 
-################## Xbee Config ########################################
+######################################## Xbee Config #####################################################
 PORT = "/dev/ttyUSB0"
 BAUD_RATE = 57600
 REMOTE_NODE_ID = "MAAT2"
 DATA_BITS = serial.EIGHTBITS
-STOP_BITS=serial.STOPBITS_ONE
-PARITY=serial.PARITY_NONE
-
+STOP_BITS = serial.STOPBITS_ONE
+PARITY = serial.PARITY_NONE
 device = XBeeDevice(PORT, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
-########################################################################
+ser=serial.Serial(PORT, BAUD_RATE)
+##########################################################################################################
 
 ######################################## GPS Function ####################################################
-import time
-import re 
-    
 def dm_to_sd(dm):
     '''
     Converts a geographic co-ordinate given in "degrees/minutes" dddmm.mmmm
@@ -82,7 +76,7 @@ def GGA(line):
 
     return res_lat,lat_dir,res_lon,lon_dir,res_alt
 
-#################################################################################################
+######################################## Sensors Function ####################################################
 # Fungsi membaca hasil akselerasi 
 def get_acceleration(acc_object):
     (x,y,z) = acc_object.getAxes()
@@ -106,7 +100,9 @@ def get_heading(magnet_object):
     # perlu ditambahin sudut deklinasi mangetik supaya lebih akurat 
     # dan konfigurasi setting magnetometer
 
-    return heading * 180/PI
+    heading = heading * 180/PI
+    
+    return heading
       
 # Fungsi membaca sensor GY-85
 def get_GY85(result_GY85, queue_signal):
@@ -123,26 +119,6 @@ def get_GY85(result_GY85, queue_signal):
             result_GY85.put((acc_coord, gyro_coord, heading))
             sleep(0.1)
 
-# def split_sentence():
-#     sentence =  "$GPGGA,12345.07,1929.045,S,02410.506,E,1,04,2.6,100.0,M,-33.9,M,,0000*6D"\
-#     "$G$NGGA,11111.07,1929.045,S,02410.506,E,1,04,2.6,100.0,M,-33.9,M,,0000*6D"\
-#     "$GPGGA,12222.07,1929.045,S,02410.506,E,1,04,2.6,100.0,M,-33.9,M,,0000*6D"\
-#     "$GACCA,555603.07,1929.045,S,02410.506,E,1,04,2.6,100.0,M,-33.9,M,,0000*6D"
-
-#     # 13 commas in a valid input
-#     # should return 15 in length
-#     valid_data = []
-#     pattern = re.compile(r'\$G(N|P)GGA')
-#     matches = pattern.finditer(sentence)
-
-#     # "$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.0,M,-33.9,M,,0000*6D"
-#     for match in matches:
-#         index = match.span()[0] # start index
-#         split_data = sentence[index:].split(',')
-#         valid_data.append(split_data[index:index+15])
-#         return valid_data
-    
-        
 # Fungsi membaca hasil sensor GPS dan mendekode hasil GPS
 def gps(result_gps, queue_signal):  
     ser = serial.Serial ("/dev/ttyS0",115200,bytesize = serial.EIGHTBITS,
@@ -158,39 +134,35 @@ def gps(result_gps, queue_signal):
 
                 lat,lat_dir,lon,lon_dir,alt=GGA(my_sentence)
 
-                res_x = "%.4f" % (lat) 
-                res_y = "%.4f" % (lon) 
-                z = "%.2f" % alt 
-
-                result_gps.put(res_x)
-                result_gps.put(res_y)
-                result_gps.put(z)
-  
-         
-##########################################
+                result_gps.put((lat, lon, alt))
+        
+##############################################################################################################
 # Fungsi ambil gambar, kompres dan kirim 
 def take_picture(queue_signal):
     while True:
         # if receive a signal then start the camera
         if (not queue_signal.empty()):
-            print("ignore sensor")
-            print("start camera")
+            print("[*] Ignore sensor")
+            print("[+] start camera")
             camera=PiCamera()
             camera.resolution = (1280,720)
             camera.start_preview()
             
             sleep(2)
-            
-            output = np.empty((720, 1280, 3), dtype=np.uint8)
+
+            # bikin array numpy
+            output = empty((720, 1280, 3), dtype=uint8)
             camera.capture(output, 'rgb')
-    
+
+            # encode 
             data = simplejpeg.encode_jpeg(output, 90, "RGB", "444") # return bytes
-            # -- kode buat send xbee ---
+    
+            ser.write(data)
             
             camera.stop_preview()
             camera.close()
-            print("TAKE GAMBAR")
-            #send(ouput)
+            print("[+] AMBIL GAMBAR")
+            
             queue_signal.get() # empty the queue
 
         
@@ -207,81 +179,115 @@ def send_data(DATA_TO_SEND):
     device.send_data_async(remote_device, DATA_TO_SEND)
     print("Success")
     
+def data_receive_callback(xbee_message):
+    # Instantiate a remote XBee device object.
+    xbee_network = device.get_network()
+    remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
+    #remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A200XXXXXX"))
+    
+    # Read data sent by the remote device.
+    xbee_message = device.read_data(remote_device)
+    
 # Main Thread           
 if __name__ == "__main__":
-    #device.open()
-    i = 0 # data masuk keberapa
-    once = 0
-    flight=0.0
+    # ---------- init variabel ----------
+    #Xbee
+    '''
+    device.open()
+    #remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A200XXXXXX"))
+    xbee_network = device.get_network()
+    remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
+    '''
+    #
+    
+    no_data = 0  # data masuk keberapa
+    once = True  # default value True
+    flight_time = 0.0
+    M_PI=3.14159265358979323846
+    
     result_GY85 = multiprocessing.Queue()
     result_gps = multiprocessing.Queue()
     ground_countrol_signal = multiprocessing.Queue() 
-    ground_countrol_signal.put(1)
+    #ground_countrol_signal.put(1)
     #initial_time=time.time()
-    
-    M_PI=3.14159265358979323846
     
     print("Starting process")
     
-    pGY85 = multiprocessing.Process(target=get_GY85, args=(result_GY85, ground_countrol_signal)) # thread pembaca GY85
-    pgps = multiprocessing.Process(target=gps, args=(result_gps, ground_countrol_signal))        # thread pembaca gps
+    # init thread
+    pGY85 = multiprocessing.Process(target=get_GY85, args=(result_GY85, ground_countrol_signal)) 
+    pgps = multiprocessing.Process(target=gps, args=(result_gps, ground_countrol_signal))        
     p_camera = multiprocessing.Process(target=take_picture, args=(ground_countrol_signal, ))     
     
-    pGY85.start() # mulai thread GY85
-    pgps.start() # mulai thread gps
+    # mulai thread sensor dan camera
+    pGY85.start() 
+    #pgps.start() 
     p_camera.start()
     
-    # kelanjutannya, semua thread harus di sinkronisasi, untuk accelerometer harus di ketahui tipe datanya agar bisa disesuaikan di 
-    # list acc. Thread pembacaan kamera harus dibuat dan thread prosesing data ke zigbee dibuat di main saja
+    # Thread pembacaan kamera harus dibuat dan thread prosesing data ke zigbee dibuat di main saja
     print("Starting encoding and transmission")
     while True:
         try:
-            acc_data, gyro_data, heading_data = result_GY85.get()
-            accelerationX, accelerationY, accelerationZ = acc_data
-            gyroX, gyroY, gyroZ = gyro_data
-            
-            roll = 180 * atan(accelerationY/sqrt(accelerationX**2 + accelerationZ**2))/M_PI
-            pitch = 180 * atan(accelerationX/sqrt(accelerationY**2 + accelerationZ**2))/M_PI
-            yaw = 180 * atan(accelerationZ/sqrt(accelerationX**2 + accelerationZ**2))/M_PI
-            
-            #lat = result_gps.get()
-            #lon = result_gps.get()
-            #alt = result_gps.get()
-            
-            if (once == 0 and sqrt(pow(accelerationX,2)+pow(accelerationY,2)+pow(accelerationZ,2))>1,25):
-                initial_time=time.time()
-                once==1
-            
-            if (once == 1):
-                flight=time.strftime("%M.%S", time.gmtime(time.time() - initial_time))
-            
-            # data sensor, alt,lon dan lat 4 decimal point
-            #arr_datas = [flight, alt, lon, lat, 0, 0, 0, heading_data, roll, pitch, yaw, accelerationX, accelerationY, accelerationZ, gyroX, gyroY, gyroZ]         
-            arr_datas = [0.0, 0.0, 0.0, heading_data, roll, pitch, yaw, accelerationX, accelerationY, accelerationZ, gyroX, gyroY, gyroZ] 
-            
-            # data = index + sensor + checksum
-            data = "%s,%.4f" % (",".join([f"{i:.2f}" for i in arr_datas]), sum(arr_datas))
-            data = f"$RKT,{i},{flight},{data},{len(data)},<CR><LF>"
-            
-            print(data)
-            #send_data(data)
-            
-            """
-            # pseudo code for receiving signal from ground control
-            if (signal_is_not_empty): 
-                queue_signal.append(signal) # thread will call the camera function
-                                            # and pop the value for the next incoming signal
-            """
+            #xbee_message = device.read_data(remote_device)
+            if ser.in_waiting > 0:
+                ser.readline()
+                ground_countrol_signal.put(1)
+            else: 
+                acc_data, gyro_data, heading_data = result_GY85.get()
+                
+                accelerationX, accelerationY, accelerationZ = acc_data
+                gyroX, gyroY, gyroZ = gyro_data
+                
+                roll = 180 * atan(accelerationY/sqrt(accelerationX**2 + accelerationZ**2))/M_PI
+                pitch = 180 * atan(accelerationX/sqrt(accelerationY**2 + accelerationZ**2))/M_PI
+                yaw = 180 * atan(accelerationZ/sqrt(accelerationX**2 + accelerationZ**2))/M_PI
+                
+                #lat, lon, alt = result_gps.get()
+                lat=0.0;lon=0.0;alt=0.0
+
+                # jika sudah melebihi 1.25G mulai record flight time
+                if (once and sqrt(pow(accelerationX,2)+pow(accelerationY,2)+pow(accelerationZ,2))>1.25):
+                    initial_time = time.time()
+                    once = False
+                
+                if (not once):
+                    flight_time = float(strftime("%M.%S", gmtime(time.time() - initial_time)))
+
+                # ------ format data ------
+                checksum = no_data+flight_time+alt+lon+lat+0.0+0.0+0.0+heading_data+roll+pitch+yaw+accelerationX+accelerationY+accelerationZ+gyroX+gyroY+gyroZ
+                data = "$RKT,%d,%s,%0.2f,%0.4f,%0.4f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f"%(no_data,flight_time,alt,lon,lat,0.0,0.0,0.0,heading_data,roll,pitch,yaw,accelerationX,accelerationY,accelerationZ,gyroX,gyroY,gyroZ,checksum)
+                
+                # data = "%s,%.4f,%s<CR><LN>" % (data, checksum, len(data)+str(len(checksum)) + 2 + 8) #Cek footernya diitung berapa karakter
+                data = "%s,%s\r\n" % (data, len(data) + 2 + 2) # len + koma + footer
+                """
+                data_sensor = [no_data, flight_time, alt, lon, lat, 0.0, 0.0, 0.0, heading_data, roll, pitch, yaw,    
+                               accelerationX, accelerationY, accelerationZ, gyroX, gyroY, gyroZ]
+
+                checksum = round(sum(data_sensor), 4)
+                data = "$RKT,%s,%s" % (','.join(map(str, data_sensor)), checksum)
+                #data = "$RKT,%s,%s\r\n" % (','.join(map(str, data_sensor)), checksum) # Yang ini kah?
+                data = "%s%s\n\r" % (data, len(data)) # sisipkan world length ke data
+                """
+                # kalau ada sinyal dari ground control stop sending data sensor
+                
+                    #device.send_data_async(remote_device, data)
+                #send_data(data)                                  
+                print(data)
+                ser.write(str.encode(data))
+                no_data += 1
             # pseudo ground control station
-            #if (ground_countrol_signal.empty() == True):
+            '''
+            if (xbee_message == 'cam'):
                 # imitate a signal
-            #    ground_countrol_signal.put(1)
-            
-            i += 1
+                ground_countrol_signal.put(1)
+            '''
+      
             sleep(0.1)
+        
+            
         except KeyboardInterrupt:
-            print("Ending Transmission and process")
+            print("\nEnding Transmission and process")
             pGY85.join()
             pgps.join()
+            p_camera.join()
             #device.close()
             exit(0)
